24201dd95d649a2b947f13f512080b61ef8be749
amitchakradhari <amitchakradhari2406@gmail.com>
2021-03-08 21:49:53 +0530

    added gif file

--- Custom Gauge Control using UIKIT/ViewController.swift
+++ Custom Gauge Control using UIKIT/ViewController.swift
@@ -7,7 +7,7 @@ class ViewController: UIViewController {
     var gaugeView: GaugeView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        gaugeView = GaugeView(frame: CGRect(x: 40, y: 40, width: 256, height: 256))
+        gaugeView = GaugeView(frame: CGRect(x: view.bounds.width/2 - 128, y: view.bounds.height/2 - 128, width: 256, height: 256))
         gaugeView.backgroundColor = .clear
         view.addSubview(gaugeView)
     }
